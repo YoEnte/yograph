fd699ecb684c22d8c3196760da991ac6a45e2800
YoEnte <67060214+YoEnte@users.noreply.github.com>
2023-04-29 14:21:55 +0200

    meth commit

---
+++ math.py
@@ -0,0 +1,35 @@
+from socha import *
+import random
+import math
+import time
+
+class Logic(IClientHandler):
+
+    gameState: GameState
+
+    def __init__(self) -> None:
+        pass
+
+    def calculate_move(self) -> Move:
+
+        test_vecs = [
+            Vector(1, -1),
+            Vector(5, -5),
+            Vector(20, 0),
+            Vector(-11, -1),
+            Vector(-51, -51),
+        ]
+
+        for v in test_vecs:
+            print(v, v.magnitude, (v / v.magnitude))
+
+
+        return random.choice(self.gameState.possible_moves)
+
+    def on_update(self, state: GameState):
+        self.gameState = state
+
+# ////////////////    Starter    ////////////////
+        
+if __name__ == "__main__":
+    Starter(Logic())
